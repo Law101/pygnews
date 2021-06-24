151be271fb7e078a291589e1684ab10b75fc10e7
law101 <lawrencebolu@gmail.com>
2021-06-24 10:23:38 +0100

    Added comments and doctstrings

--- src/pygnews/helpers.py
+++ src/pygnews/helpers.py
@@ -1,13 +1,25 @@
+# Import Neccessary Libraries
 import requests
 import urllib
 import feedparser
 from bs4 import BeautifulSoup
-# import dateparser
+import dateparser
+
 # import json
 
 
 def set_ceid(country="US", language="en"):
-    """Set Country and Language parameters"""
+
+    """Set the base country and language for the RSS Feed
+
+    Args:
+        country (str, optional): Country abbreviation. Defaults to "US".
+        language (str, optional): Country Language abbreviation. Defaults to "en".
+
+    Returns:
+        ceid (str): acceptable RSS feed query string.
+    """
+
     ceid = "?ceid={country}:{language}&hl={language}&gl={country}".format(
         country=country, language=language
     )
@@ -15,7 +27,17 @@ def set_ceid(country="US", language="en"):
 
 
 def feed_parser(feedURL):
-    """Use Feedparser to Extract Feeds"""
+    """Use Feedparser to Extract Feeds from the specified URL.
+
+    Args:
+        feedURL (str): URL to extract feeds from
+
+    Raises:
+        Exception: when feed is not available.
+
+    Returns:
+        dict: a dictionary object with feed and entries as keys.
+    """
 
     # Check if it's a supported URL
     request_feed = requests.get(feedURL)
@@ -29,7 +51,14 @@ def feed_parser(feedURL):
 
 
 def topStories_parser(text_dict):
-    """Return subarticles from the main and topic feeds"""
+    """Extracts relevant infomation from feed Entries
+
+    Args:
+        text_dict (dict): feed Entity to extract information from.
+
+    Returns:
+        coverage (dict): a dictionary containing extracted information.
+    """
 
     summary = text_dict["summary"]
     bs4_html = BeautifulSoup(summary, "html.parser")
@@ -69,10 +98,19 @@ def topStories_parser(text_dict):
 
 
 def collect_coverage(entries):
+    """Extract sub news and collate them together.
+
+    Args:
+        entries (dict): feed entries
+
+    Returns:
+        entries_dict (dict): news coverage collection
+    """
 
     entries_dict = dict()
-    for i, val in enumerate(entries):
+    for i, _ in enumerate(entries):
         if "summary" in entries[i].keys():
+            # Use topStories_parser to extract information from entries
             entries_dict[i] = topStories_parser(entries[i])
         else:
             print("News Summary Not Available")
@@ -81,11 +119,30 @@ def collect_coverage(entries):
 
 
 def search_parser(search_query):
+    """Construct URL from search query
+
+    Args:
+        search_query (str): Search Querry supplied by user.
+
+    Returns:
+        parsed_url (str): Passed URL
+    """
     parsed_url = urllib.parse.quote_plus(search_query)
     return parsed_url
 
 
 def date_parser(date=None):
+    """Validate date and extract Year, Month and Day
+
+    Args:
+        date (str, optional): Date to be parsed. Defaults to None.
+
+    Raises:
+        Exception: When invalid date pattern is parsed
+
+    Returns:
+        valid_date (str): Extracted Date
+    """
     try:
         valid_date = dateparser.parse(date).strftime("%Y-%m-%d")
         return str(valid_date)
